54083ec26351cf52a907b3185fee8bcf803b7917
erascong <edrascon50@gmail.com>
2019-06-25 17:58:51 -0500

    Update remove_images.sh
    rework

--- remove_images.sh
+++ remove_images.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #
-# Remove unused docker images from list.txt
+# Remove unused docker images from delete.list
+#
+# To populate delete.list run:
+# $ docker images | awk 'NR>1 {print $3}' > delete.list
+# Exclude images if necessary
 #
 i=1
-for image in `awk -F: '{print $1}' list.txt`
-do
-# replace with docker command
-  rm $image && echo "image $image deleted"
+for image in `awk -F: '{print $1}' delete.list`
+  do
+    # deleting images
+    docker rmi $image
+    # test deletion
+    # echo "image $image will be deleted"
 done
